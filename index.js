c4c960d4cc26a222ee90b11e8535d823dd98d502
Yusuke Suzuki <yusuke.suzuki.0707@gmail.com>
2017-08-05 18:38:43 +0900

    Create SPA base with packages

---
+++ index.js
@@ -0,0 +1,44 @@
+import 'babel-polyfill';
+import Koa from 'koa';
+import logger from 'koa-logger';
+import serve from 'koa-static';
+import bodyParser from 'koa-bodyparser';
+import json from 'koa-json';
+import routes from './config/routes';
+import dotenv from 'dotenv';
+import error from 'koa-json-error';
+import ApplicationError from './app/models/errors/ApplicationError';
+
+const formatError = (error) => {
+  let json;
+  if (error instanceof ApplicationError) {
+    json = {
+      status: error.status,
+      title: error.name,
+      detail: error.message
+    };
+  } else {
+    json = {
+      status: 500,
+      title: 'Error',
+      detail: 'The application has encountered an unknown error.'
+    };
+  }
+  console.log(json);
+  return json;
+}
+
+const app = new Koa();
+
+dotenv.config();
+
+app.use(error(formatError));
+app.use(json());
+app.use(bodyParser());
+app.use(logger());
+app.use(serve(__dirname + '/public'));
+
+routes(app);
+
+app.listen(process.env.PORT || 3000);
+console.log('Starting...');
