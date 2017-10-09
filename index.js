ae8465c9babe7aef4b64adb9b5154628fb88b33c
Yusuke Suzuki <yusuke.suzuki.0707@gmail.com>
2017-10-09 22:31:08 +0900

    Render JSON error response only if API path

--- index.js
+++ index.js
@@ -6,35 +6,12 @@ import bodyParser from 'koa-bodyparser';
 import json from 'koa-json';
 import routes from './routes';
 import dotenv from 'dotenv';
-import error from 'koa-json-error';
-import ApplicationError from './app/models/errors/ApplicationError';
 import detectLocale from './app/middlewares/detectLocale';
-
-const formatError = (error) => {
-  console.log(error);
-  let json;
-  if (error instanceof ApplicationError) {
-    json = {
-      status: error.status,
-      title: error.name,
-      detail: error.message
-    };
-  } else {
-    json = {
-      status: 500,
-      title: 'Error',
-      detail: 'The application has encountered an unknown error.'
-    };
-  }
-  console.log(json);
-  return json;
-}
 
 const app = new Koa();
 
 dotenv.config();
 
-app.use(error(formatError));
 app.use(json());
 app.use(bodyParser());
 app.use(logger());
--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 import Router from 'koa-router';
 import render from 'koa-ejs';
 import path from 'path';
+import error from 'koa-json-error';
+import ApplicationError from './app/models/errors/ApplicationError';
 
 import Auth from './app/controllers/Auth';
 import Users from './app/controllers/Users';
@@ -14,6 +16,26 @@ import Follows from './app/controllers/Follows';
 import InappropriateContents from './app/controllers/InappropriateContents';
 
 import { generateMetadata } from './app/models/Utils';
+
+const formatError = (error) => {
+  console.log(error);
+  let json;
+  if (error instanceof ApplicationError) {
+    json = {
+      status: error.status,
+      title: error.name,
+      detail: error.message
+    };
+  } else {
+    json = {
+      status: 500,
+      title: 'Error',
+      detail: 'The application has encountered an unknown error.'
+    };
+  }
+  console.log(json);
+  return json;
+}
 
 const routes = (app) => {
   const router = new Router();
@@ -64,115 +86,115 @@ const routes = (app) => {
     await ctx.render('index', params);
   });
 
-  router.post('/api/auth', async (ctx, next) => {
+  router.post('/api/auth', error(formatError), async (ctx, next) => {
     const auth = new Auth;
     ctx.body = await auth.create(ctx.request.body);
   });
 
-  router.post('/api/devices', async (ctx, next) => {
+  router.post('/api/devices', error(formatError), async (ctx, next) => {
     const devices = new Devices;
     ctx.body = await devices.create(ctx.request.headers.authorization, ctx.request.body);
     ctx.status = 204;
   });
 
-  router.get('/api/users/:userId', async (ctx, next) => {
+  router.get('/api/users/:userId', error(formatError), async (ctx, next) => {
     const users = new Users;
     ctx.body = await users.show(ctx.request.headers.authorization, ctx.params.userId);
   });
 
-  router.del('/api/users/:userId', async (ctx, next) => {
+  router.del('/api/users/:userId', error(formatError), async (ctx, next) => {
     const users = new Users;
     await users.delete(ctx.request.headers.authorization, ctx.params.userId);
     ctx.status = 204;
   });
 
-  router.get('/api/users/:userId/maps', async (ctx, next) => {
+  router.get('/api/users/:userId/maps', error(formatError), async (ctx, next) => {
     const userMaps = new UserMaps;
     ctx.body = await userMaps.index(ctx.request.headers.authorization, ctx.params.userId);
   });
 
-  router.get('/api/maps', async (ctx, next) => {
+  router.get('/api/maps', error(formatError), async (ctx, next) => {
     const maps = new Maps;
     ctx.body = await maps.index(ctx.request.headers.authorization, ctx.query);
   });
 
-  router.get('/api/maps/:mapId', async (ctx, next) => {
+  router.get('/api/maps/:mapId', error(formatError), async (ctx, next) => {
     const maps = new Maps;
     ctx.body = await maps.show(ctx.request.headers.authorization, ctx.params.mapId);
   });
 
-  router.post('/api/maps', async (ctx, next) => {
+  router.post('/api/maps', error(formatError), async (ctx, next) => {
     const maps = new Maps;
     ctx.body = await maps.create(ctx.request.headers.authorization, ctx.request.body);
   });
 
-  router.put('/api/maps/:mapId', async (ctx, next) => {
+  router.put('/api/maps/:mapId', error(formatError), async (ctx, next) => {
     const maps = new Maps;
     ctx.body = await maps.update(ctx.request.headers.authorization, ctx.request.body, ctx.params.mapId);
   });
 
-  router.del('/api/maps/:mapId', async (ctx, next) => {
+  router.del('/api/maps/:mapId', error(formatError), async (ctx, next) => {
     const maps = new Maps;
     await maps.delete(ctx.request.headers.authorization, ctx.params.mapId);
     ctx.status = 204;
   });
 
-  router.get('/api/maps/:mapId/collaborators', async (ctx, next) => {
+  router.get('/api/maps/:mapId/collaborators', error(formatError), async (ctx, next) => {
     const collaborators = new Collaborators;
     ctx.body = await collaborators.index(ctx.request.headers.authorization, ctx.params.mapId);
   });
 
-  router.get('/api/maps/:mapId/spots', async (ctx, next) => {
+  router.get('/api/maps/:mapId/spots', error(formatError), async (ctx, next) => {
     const spots = new Spots;
     ctx.body = await spots.index(ctx.request.headers.authorization, ctx.params.mapId, ctx.currentLocale);
   });
 
-  router.get('/api/maps/:mapId/spots/:spotId', async (ctx, next) => {
+  router.get('/api/maps/:mapId/spots/:spotId', error(formatError), async (ctx, next) => {
     const spots = new Spots;
     ctx.body = await spots.show(ctx.request.headers.authorization, ctx.params.mapId, ctx.params.spotId, ctx.currentLocale);
   });
 
-  router.post('/api/maps/:mapId/reviews', async (ctx, next) => {
+  router.post('/api/maps/:mapId/reviews', error(formatError), async (ctx, next) => {
     const reviews = new Reviews;
     ctx.body = await reviews.create(ctx.request.headers.authorization, ctx.params.mapId, ctx.request.body);
   });
 
-  router.get('/api/reviews', async (ctx, next) => {
+  router.get('/api/reviews', error(formatError), async (ctx, next) => {
     const reviews = new Reviews;
     ctx.body = await reviews.index(ctx.request.headers.authorization, ctx.currentLocale, null, ctx.query);
   });
 
-  router.put('/api/reviews/:reviewId', async (ctx, next) => {
+  router.put('/api/reviews/:reviewId', error(formatError), async (ctx, next) => {
     const reviews = new Reviews;
     ctx.body = await reviews.update(ctx.request.headers.authorization, ctx.params.reviewId, ctx.request.body);
   });
 
-  router.del('/api/reviews/:reviewId', async (ctx, next) => {
+  router.del('/api/reviews/:reviewId', error(formatError), async (ctx, next) => {
     const reviews = new Reviews;
     ctx.body = await reviews.delete(ctx.request.headers.authorization, ctx.params.reviewId);
   });
 
-  router.get('/api/maps/:mapId/reviews', async (ctx, next) => {
+  router.get('/api/maps/:mapId/reviews', error(formatError), async (ctx, next) => {
     const reviews = new Reviews;
     ctx.body = await reviews.index(ctx.request.headers.authorization, ctx.currentLocale, ctx.params.mapId, ctx.query);
   });
 
-  router.get('/api/maps/:mapId/reviews/:reviewId', async (ctx, next) => {
+  router.get('/api/maps/:mapId/reviews/:reviewId', error(formatError), async (ctx, next) => {
     const reviews = new Reviews;
     ctx.body = await reviews.show(ctx.request.headers.authorization, ctx.currentLocale, ctx.params.mapId, ctx.params.reviewId);
   });
 
-  router.post('/api/maps/:mapId/follow', async (ctx, next) => {
+  router.post('/api/maps/:mapId/follow', error(formatError), async (ctx, next) => {
     const follows = new Follows;
     ctx.body = await follows.create(ctx.request.headers.authorization, ctx.params.mapId);
   });
 
-  router.del('/api/maps/:mapId/follow', async (ctx, next) => {
+  router.del('/api/maps/:mapId/follow', error(formatError), async (ctx, next) => {
     const follows = new Follows;
     ctx.body = await follows.destroy(ctx.request.headers.authorization, ctx.params.mapId);
   });
 
-  router.post('/api/inappropriate_contents', async (ctx, next) => {
+  router.post('/api/inappropriate_contents', error(formatError), async (ctx, next) => {
     const inappropriateContents = new InappropriateContents;
     await inappropriateContents.create(ctx.request.headers.authorization, ctx.request.body);
     ctx.status = 204;
