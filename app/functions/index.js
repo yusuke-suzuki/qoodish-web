24a4b9d5f0acd7f7882f4cd19ca0064032b140e4
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-03-19 21:41:30 +0900

    Add function to generate thumbnails

--- app/functions/index.js
+++ app/functions/index.js
@@ -1,6 +1,11 @@
+const gcs = require('@google-cloud/storage')();
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
 const functions = require('firebase-functions');
 const express = require('express');
-const fs = require('fs');
 const ejs = require('ejs');
 import { isBot, generateMetadata } from './Utils';
 
@@ -32,3 +37,55 @@ app.get(pageRoutes, async (req, res) => {
 });
 
 exports.host = functions.https.onRequest(app);
+
+exports.generateThumbnail = functions.storage.bucket(process.env.FIREBASE_IMAGE_BUCKET_NAME).object().onChange((event) => {
+  const object = event.data;
+
+  const fileBucket = object.bucket;
+  const filePath = object.name;
+  const contentType = object.contentType;
+  const resourceState = object.resourceState; // The resourceState is 'exists' or 'not_exists' (for file/folder deletions).
+  const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+
+  if (!contentType.startsWith('image/')) {
+    console.log('This is not an image.');
+    return null;
+  }
+
+  const fileName = path.basename(filePath);
+  if (fileName.startsWith('thumb_')) {
+    console.log('Already a Thumbnail.');
+    return null;
+  }
+
+  if (resourceState === 'not_exists') {
+    console.log('This is a deletion event.');
+    return null;
+  }
+
+  if (resourceState === 'exists' && metageneration > 1) {
+    console.log('This is a metadata change event.');
+    return null;
+  }
+
+  const bucket = gcs.bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  const metadata = {
+    contentType: contentType
+  };
+
+  return bucket.file(filePath).download({
+    destination: tempFilePath,
+  }).then(() => {
+    console.log('Image downloaded locally to', tempFilePath);
+    return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+  }).then(() => {
+    console.log('Thumbnail created at', tempFilePath);
+    const thumbFileName = `thumb_${fileName}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    return bucket.upload(tempFilePath, {
+      destination: thumbFilePath,
+      metadata: metadata
+    });
+  }).then(() => fs.unlinkSync(tempFilePath));
+});
