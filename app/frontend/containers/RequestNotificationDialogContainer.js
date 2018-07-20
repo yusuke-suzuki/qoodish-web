a4a11822469084128c06b8df0c4a697c4b2610a7
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-07-21 01:11:34 +0900

    Fix push notification request

--- app/frontend/containers/RequestNotificationDialogContainer.js
+++ app/frontend/containers/RequestNotificationDialogContainer.js
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import RequestNotificationDialog from '../ui/RequestNotificationDialog';
-import ApiClient from './ApiClient';
 import closeRequestNotificationDialog from '../actions/closeRequestNotificationDialog';
 import firebase from 'firebase/app';
 import 'firebase/messaging';
-import fetchRegistrationToken from '../actions/fetchRegistrationToken';
 import forbidNotification from '../actions/forbidNotification';
+import openToast from '../actions/openToast';
+import fetchRegistrationToken from '../actions/fetchRegistrationToken';
+import ApiClient from '../containers/ApiClient';
+import requestStart from '../actions/requestStart';
+import requestFinish from '../actions/requestFinish';
 
 const mapStateToProps = state => {
   return {
@@ -33,17 +36,27 @@ const mapDispatchToProps = dispatch => {
       try {
         await messaging.requestPermission();
       } catch (e) {
-        console.log('Unable to get permission to notify.', e);
-      }
-      const registrationToken = await messaging.getToken();
-      if (!registrationToken) {
-        console.log('Unable to get registration token.');
+        console.log(e);
+        dispatch(openToast('Unable to get permission to notify.'));
         return;
       }
-      dispatch(closeRequestNotificationDialog());
+
+      dispatch(requestStart());
+      const registrationToken = await messaging.getToken();
+      if (!registrationToken) {
+        dispatch(openToast('Unable to get registration token.'));
+        return;
+      }
+
       const response = await client.sendRegistrationToken(registrationToken);
+      dispatch(requestFinish());
+
       if (response.ok) {
         dispatch(fetchRegistrationToken(registrationToken));
+        dispatch(closeRequestNotificationDialog());
+        dispatch(openToast('Push notification was successfully enabled.'));
+      } else {
+        dispatch(openToast('An error occured.'));
       }
     }
   };
