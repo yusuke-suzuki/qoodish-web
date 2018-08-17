b10638da5cc5a2d4efd249bab73b9455c4cf3a4d
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-08-18 00:12:52 +0900

    Fix missing of expand summary button in iOS

--- app/frontend/ui/MapBottomSeat.jsx
+++ app/frontend/ui/MapBottomSeat.jsx
@@ -7,8 +7,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import FollowMapButtonContainer from '../containers/FollowMapButtonContainer';
 
 const styles = {
@@ -17,15 +15,6 @@ const styles = {
   },
   listItem: {
     padding: 0
-  },
-  expandButtonContainer: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 116,
-    textAlign: 'center'
-  },
-  expandButton: {
-    boxShadow: '0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)'
   },
   cardContent: {
     paddingTop: 24,
@@ -53,25 +42,8 @@ class MapBottomSeat extends React.Component {
         anchor="bottom"
         open={true}
       >
-        {this.renderExpandButton()}
         {this.renderSummaryCard()}
       </Drawer>
-    );
-  }
-
-  renderExpandButton() {
-    return (
-      <div style={styles.expandButtonContainer}>
-        <Button
-          variant="fab"
-          style={styles.expandButton}
-          onClick={this.props.handleShowTimelineClick}
-          mini={true}
-          color="primary"
-        >
-          <ExpandLessIcon />
-        </Button>
-      </div>
     );
   }
 
