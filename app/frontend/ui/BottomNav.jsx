a4e4a072335cfcd1377b54c2b1913213076b95aa
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-10-24 23:29:52 +0900

    Remove labels on Bottom Nav

--- app/frontend/ui/BottomNav.jsx
+++ app/frontend/ui/BottomNav.jsx
@@ -9,7 +9,6 @@ import MapIcon from '@material-ui/icons/Map';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Badge from '@material-ui/core/Badge';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Typography from '@material-ui/core/Typography';
 import I18n from '../containers/I18n';
 
 const styles = {
@@ -21,10 +20,8 @@ const styles = {
   },
   bottomAction: {
     width: '20%',
-    minWidth: 'auto'
-  },
-  label: {
-    fontSize: 'small'
+    minWidth: 'auto',
+    paddingTop: 8
   },
   link: {
     textDecoration: 'none',
@@ -73,21 +70,11 @@ export default class BottomNav extends React.PureComponent {
   render() {
     return (
       <Paper style={styles.bottomNav} elevation={20}>
-        <BottomNavigation showLabels value={this.state.tabValue}>
+        <BottomNavigation value={this.state.tabValue}>
           <BottomNavigationAction
             component={Link}
             to="/"
             title={I18n.t('home')}
-            label={
-              <Typography
-                variant="body2"
-                color="inherit"
-                noWrap
-                style={styles.label}
-              >
-                {I18n.t('home')}
-              </Typography>
-            }
             icon={<HomeIcon />}
             style={styles.bottomAction}
           />
@@ -95,16 +82,6 @@ export default class BottomNav extends React.PureComponent {
             component={Link}
             to="/discover"
             title={I18n.t('discover')}
-            label={
-              <Typography
-                variant="body2"
-                color="inherit"
-                noWrap
-                style={styles.label}
-              >
-                {I18n.t('discover')}
-              </Typography>
-            }
             icon={<ExploreIcon />}
             style={styles.bottomAction}
           />
@@ -112,16 +89,6 @@ export default class BottomNav extends React.PureComponent {
             component={Link}
             to="/maps"
             title={I18n.t('maps')}
-            label={
-              <Typography
-                variant="body2"
-                color="inherit"
-                noWrap
-                style={styles.label}
-              >
-                {I18n.t('maps')}
-              </Typography>
-            }
             icon={<MapIcon />}
             style={styles.bottomAction}
           />
@@ -129,16 +96,6 @@ export default class BottomNav extends React.PureComponent {
             component={Link}
             to="/profile"
             title={I18n.t('account')}
-            label={
-              <Typography
-                variant="body2"
-                color="inherit"
-                noWrap
-                style={styles.label}
-              >
-                {I18n.t('account')}
-              </Typography>
-            }
             icon={<AccountCircleIcon />}
             style={styles.bottomAction}
           />
@@ -146,16 +103,6 @@ export default class BottomNav extends React.PureComponent {
             component={Link}
             to="/notifications"
             title={I18n.t('notice')}
-            label={
-              <Typography
-                variant="body2"
-                color="inherit"
-                noWrap
-                style={styles.label}
-              >
-                {I18n.t('notice')}
-              </Typography>
-            }
             icon={this.renderNotificationIcon()}
             style={styles.bottomAction}
           />
