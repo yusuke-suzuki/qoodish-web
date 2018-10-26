48f8b94523efcfed829595167bc331aa3f35b38b
Yusuke <yusuke.suzuki.0707@gmail.com>
2018-10-26 17:37:38 +0900

    Recover sw.js

---
+++ functions/hosting/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install');
+});
+ self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate');
+});
+ self.addEventListener('fetch', (e) => {
+  //console.log(e.request.url);
+});
