c1904c6849fc40004622f774525d97a2afb172e4
Yusuke <yusuke.suzuki.0707@gmail.com>
2019-03-06 00:05:23 +0900

    New Map Card

--- src/frontend/components/molecules/MapCard.js
+++ src/frontend/components/molecules/MapCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Link from '../molecules/Link';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
+
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import LockIcon from '@material-ui/icons/Lock';
+
+import Link from '../molecules/Link';
 import FollowMapButton from '../molecules/FollowMapButton';
+import I18n from '../../utils/I18n';
 
 const styles = {
   mapImage: {
@@ -15,20 +20,40 @@ const styles = {
   cardContent: {
     padding: 0,
     marginBottom: 24
+  },
+  privateIcon: {
+    fontSize: '1rem'
+  },
+  cardMedia: {
+    width: '100%'
+  },
+  buttonBase: {
+    width: '100%'
   }
 };
 
 const MapCard = props => {
+  const large = useMediaQuery('(min-width: 600px)');
   const map = props.map;
 
   return (
     <CardContent style={styles.cardContent}>
-      <ButtonBase component={Link} to={`/maps/${map.id}`} title={map.name}>
-        <CardMedia>
-          <img src={map.image_url} alt={map.name} style={styles.mapImage} />
+      <ButtonBase
+        component={Link}
+        to={`/maps/${map.id}`}
+        title={map.name}
+        style={styles.buttonBase}
+      >
+        <CardMedia style={styles.cardMedia}>
+          <img
+            src={large ? map.image_url : map.thumbnail_url}
+            alt={map.name}
+            style={styles.mapImage}
+          />
         </CardMedia>
       </ButtonBase>
       <Typography variant="subtitle1" noWrap>
+        {map.private && <LockIcon style={styles.privateIcon} color="inherit" />}{' '}
         {map.name}
       </Typography>
       <Typography variant="subtitle2" color="textSecondary" noWrap>
@@ -36,6 +61,9 @@ const MapCard = props => {
       </Typography>
       <Typography variant="subtitle2" color="textSecondary" gutterBottom noWrap>
         {map.description}
+      </Typography>
+      <Typography variant="subtitle2" color="textSecondary" gutterBottom noWrap>
+        {map.followers_count} {I18n.t('followers')}
       </Typography>
       <FollowMapButton currentMap={map} />
     </CardContent>
