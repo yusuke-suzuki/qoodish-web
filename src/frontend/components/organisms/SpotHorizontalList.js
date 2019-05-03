d9dd4a7f2b61a8111fd6b02c4abb5996029d58f0
Yusuke <yusuke.suzuki.0707@gmail.com>
2019-05-03 13:55:16 +0900

    Show reviewers on spot horizontal list

--- src/frontend/components/organisms/SpotHorizontalList.js
+++ src/frontend/components/organisms/SpotHorizontalList.js
@@ -4,7 +4,8 @@ import {
   GridList,
   GridListTile,
   GridListTileBar,
-  Typography
+  Typography,
+  Avatar
 } from '@material-ui/core';
 import selectSpot from '../../actions/selectSpot';
 import openSpotCard from '../../actions/openSpotCard';
@@ -24,7 +25,38 @@ const styles = {
   gridList: {
     flexWrap: 'nowrap',
     transform: 'translateZ(0)'
+  },
+  reviewerContainer: {
+    display: 'flex',
+    position: 'absolute',
+    zIndex: 1,
+    right: 16,
+    top: 8
+  },
+  reviewerAvatar: {
+    width: 24,
+    height: 24,
+    marginRight: -10.66666667,
+    borderColor: '#fff',
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    borderStyle: 'solid',
+    float: 'right',
+    borderWidth: 1,
+    cursor: 'pointer'
   }
+};
+
+const Reviewers = props => {
+  return props.spot.reviews
+    .slice(0, 3)
+    .map(review => (
+      <Avatar
+        src={review.author.profile_image_url}
+        alt={review.author.name}
+        style={styles.reviewerAvatar}
+      />
+    ));
 };
 
 const SpotHorizontalList = () => {
@@ -59,6 +91,9 @@ const SpotHorizontalList = () => {
               key={spot.place_id}
               onClick={() => handleSpotClick(spot)}
             >
+              <div style={styles.reviewerContainer}>
+                <Reviewers spot={spot} />
+              </div>
               <img src={spot.image_url} alt={spot.name} />
               <GridListTileBar
                 title={
