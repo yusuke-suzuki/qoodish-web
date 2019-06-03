75498541ed122b93127108ba3a465b7c7b1b7c17
Yusuke <yusuke.suzuki.0707@gmail.com>
2019-06-03 17:58:23 +0900

    Introduce Rendertron

--- functions/server/index.js
+++ functions/server/index.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const ejs = require('ejs');
 const fs = require('fs');
 const http = require('http');
+const fetch = require('node-fetch');
+const url = require('url');
 
 const isBot = require(__dirname + '/utils/isBot');
-const generateMetadata = require(__dirname + '/utils/generateMetadata');
+
+const generateUrl = req => {
+  return url.format({
+    protocol: 'https',
+    host: process.env.SITE_DOMAIN,
+    pathname: req.originalUrl
+  });
+};
 
 const app = express();
 app.use(express.static(__dirname + '/../hosting'));
@@ -29,15 +37,17 @@ const pageRoutes = [
 app.get(pageRoutes, async (req, res) => {
   if (isBot(req)) {
     console.log(`Bot access: ${req.headers['user-agent']}`);
-    const metadata = await generateMetadata(req);
-    res.status(200).send(
-      ejs.render(fs.readFileSync(__dirname + '/metadata.html').toString(), {
-        metadata: metadata,
-        fbAppId: process.env.FB_APP_ID
-      })
+
+    const response = await fetch(
+      `${process.env.RENDERTRON_ENDPOINT}/render/${generateUrl(req)}`
     );
+    const body = await response.text();
+    //res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    res.set('Vary', 'User-Agent');
+    res.send(body.toString());
   } else {
     //res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    res.set('Vary', 'User-Agent');
     res
       .status(200)
       .send(fs.readFileSync(__dirname + '/../hosting/index.html').toString());
