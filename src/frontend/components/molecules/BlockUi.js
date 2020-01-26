df6f9ae027e5fce25b568d46927adbc5bf39528b
Yusuke <yusuke.suzuki.0707@gmail.com>
2020-01-26 23:42:33 +0900

    Improve back drop

--- src/frontend/components/molecules/BlockUi.js
+++ src/frontend/components/molecules/BlockUi.js
@@ -1,39 +1,28 @@
 import React, { useCallback } from 'react';
 import { useMappedState } from 'redux-react-hook';
+import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = {
-  blockUi: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: '100%',
-    width: '100%',
-    zIndex: 2000,
-    cursor: 'wait',
-    opacity: '0.5',
-    backgroundColor: 'white'
-  },
-  progress: {
-    textAlign: 'center',
-    marginTop: '50vh'
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 10000,
+    color: '#fff'
   }
-};
+}));
 
 const BlockUi = () => {
+  const classes = useStyles();
+
   const blocking = useMappedState(
     useCallback(state => state.shared.blocking, [])
   );
 
-  return blocking ? (
-    <div style={styles.blockUi}>
-      <div style={styles.progress}>
-        <CircularProgress />
-      </div>
-    </div>
-  ) : null;
+  return (
+    <Backdrop className={classes.backdrop} open={blocking}>
+      <CircularProgress color="primary" />
+    </Backdrop>
+  );
 };
 
 export default React.memo(BlockUi);
